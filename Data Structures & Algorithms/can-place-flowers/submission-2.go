597c393a44455fe4183410cafde9340aa78844e1
Fahrul Razi <11134396+mfrazi@users.noreply.github.com>
2026-06-05 00:43:41 +0700

    Add: can-place-flowers - submission-2

---
+++ Data Structures & Algorithms/can-place-flowers/submission-2.go
@@ -0,0 +1,15 @@
+func canPlaceFlowers(flowerbed []int, n int) bool {
+    flowerLeft := n
+
+	for i:=0; i<len(flowerbed) && flowerLeft > 0; i++ {
+		leftEmpty := i == 0 || flowerbed[i-1] == 0
+		rightEmpty := i == len(flowerbed) - 1 || flowerbed[i+1] == 0
+	
+		if flowerbed[i] == 0 && leftEmpty && rightEmpty {
+			flowerbed[i] = 1
+			flowerLeft--
+		}
+	}
+
+	return flowerLeft == 0
+}
