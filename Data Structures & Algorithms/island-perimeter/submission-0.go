1681d75e8ed0cd43ff9d55c8e94f7d2dfa301be9
Fahrul Razi <11134396+mfrazi@users.noreply.github.com>
2026-06-05 10:06:58 +0700

    Add: island-perimeter - submission-0

---
+++ Data Structures & Algorithms/island-perimeter/submission-0.go
@@ -0,0 +1,27 @@
+func islandPerimeter(grid [][]int) int {
+    perimeter := 0
+
+    for i:=0; i<len(grid); i++ {
+        for j:=0; j<len(grid[0]); j++ {
+            if grid[i][j] != 1{
+                continue
+            }
+            
+            if i-1 < 0 || grid[i-1][j] == 0 {
+                perimeter++
+            } 
+            if i+1 == len(grid) || grid[i+1][j] == 0 {
+                perimeter++
+            }
+            if j-1 < 0 || grid[i][j-1] == 0 {
+                perimeter++
+            }
+            if j+1 == len(grid[0]) || grid[i][j+1] == 0 {
+                perimeter++
+            }
+        }
+    }
+
+
+    return perimeter
+}
