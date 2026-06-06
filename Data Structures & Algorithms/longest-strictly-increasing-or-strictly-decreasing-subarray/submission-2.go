12f49f4d31fa87648e838ad1c3c75d9180de45c3
Fahrul Razi <11134396+mfrazi@users.noreply.github.com>
2026-06-07 02:37:14 +0700

    Add: longest-strictly-increasing-or-strictly-decreasing-subarray - submission-2

---
+++ Data Structures & Algorithms/longest-strictly-increasing-or-strictly-decreasing-subarray/submission-2.go
@@ -0,0 +1,25 @@
+func longestMonotonicSubarray(nums []int) int {
+	inc, dec := 1, 1
+	ans := 1
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i] > nums[i-1] {
+			inc++
+			dec = 1
+		} else if nums[i] < nums[i-1] {
+			dec++
+			inc = 1
+		} else {
+			inc, dec = 1, 1
+		}
+
+		if inc > ans {
+			ans = inc
+		}
+		if dec > ans {
+			ans = dec
+		}
+	}
+
+	return ans
+}
