a5d80a80c27492dea5a84bb0fc49bcd2d6caf6f3
Fahrul Razi <11134396+mfrazi@users.noreply.github.com>
2026-06-11 10:12:12 +0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.go
@@ -0,0 +1,25 @@
+func isValid(s string) bool {
+    stack := []rune{}
+	for _, c := range s {
+		if c == '(' || c == '{' || c == '[' {
+			stack = append(stack, c)
+			continue
+		}
+
+		if len(stack) == 0 {
+			return false
+		}
+
+		top := stack[len(stack)-1]
+
+		if c == ')' && top != '(' ||
+			c == '}' && top != '{' ||
+			c == ']' && top != '[' {
+				return false
+			}
+
+		stack = stack[:len(stack)-1]
+	}
+
+	return len(stack) == 0
+}
